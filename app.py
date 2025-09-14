47e474e7a1a39390e2225b2944b09e9e2956d1a4
bharathcy <bharathcy@gmail.com>
2025-09-15 02:14:39 +0530

    updated

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def delete_todo(todo_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=PORT)
+    app.run(debug=True, host='0.0.0.0', port=PORT)
